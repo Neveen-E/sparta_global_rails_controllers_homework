90ab54793fd9df425ddb055cf9e4361281c5d357
Neveen-E <nelasar@spartaglobal.com>
2018-07-03 04:18:59 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'books#index'
+
+  get '/new', to: 'books#new'
+  post '/', to: 'books#create'
+
+  get '/:id', to: 'books#show'
+  get '/:id/edit', to: 'book#edit'
+  put '/:id', to: 'books#update'
+
+  delete '/:id', to: 'books#destroy'
 end
